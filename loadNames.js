46c7dfe227e3bcfb27c46a135a6cc79490ba1b83
karlhenriksson <153213827+karlhenriksson@users.noreply.github.com>
2023-12-07 22:54:44 +0100

    Create loadNames.js

---
+++ loadNames.js
@@ -0,0 +1,41 @@
+"use strict";
+
+/*
+
+TITTA INTE I DEN HÄR FILEN!!!
+
+Det är här alla namn sparas. Så genom att titta i den här filen kan du se vem alla ska ge till. Så för att hålla det hemligt, 
+var vänlig titta inte här!
+
+
+
+
+
+
+
+
+*/
+
+// Stores codes, along with the recipient and the person with the code
+// (keys are all lower case)
+const codeToPeople = {
+    "0102": { giver:"Ture", recipient:"Knut" },
+}
+
+// Om du är här, var vänlig spoila inte detta!  /Karl
+const easterEggs = {
+    "bånx": "Testa 'Bonx' i stället. Det är den bättre stavningen.",
+    "bonx": "Kom till Karl så får du en gratis kopp choklad. (endast de 3 första som kommer till mig får detta, så du tjänar på att hålla denna funktion hemlig tills efter att du fått din kopp)",
+    "aaa": "Varför skriker du så mycket?",
+    "aaaa": "Varför skriker du så mycket?",
+    "aaaaa": "Varför skriker du så mycket?",
+    "1234": "567×89=???",
+    "50463": "1+2+3+4+...+99+100=???",
+    "5050": "To be or not to be, that is [???]",
+    "the question": "The question is: How many days are there in February next year?",
+    "29": "<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'>https://www.desmos.com/calculator/jadlzaifbr</a>",
+    "lorem ipsum": "Dolor sit amet, consectetur adipisicing elit.",
+    "knut": "Det är jag, ja.",
+    "admin": "You have been granted admin rights. Please proceed to <a href='https://imgs.xkcd.com/comics/incident.png'>the admin control panel</a>.",
+    "password": "You have been granted admin rights. Please proceed to <a href='https://imgs.xkcd.com/comics/incident.png'>the admin control panel</a>.",
+}
