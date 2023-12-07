66dba1817fe5f6087d17c5568d87343405f878bd
karlhenriksson <153213827+karlhenriksson@users.noreply.github.com>
2023-12-07 20:21:42 +0100

    Update secretsanta.js

--- secretsanta.js
+++ secretsanta.js
@@ -10,7 +10,7 @@ const codeToPeople = {
 
 // Om du är här, var vänlig spoila inte detta!  /Karl
 const easterEggs = {
-    "bånx": "Testa 'bonx' i stället. Det är den bättre stavningen.",
+    "bånx": "Testa 'Bonx' i stället. Det är den bättre stavningen.",
     "bonx": "Kom till Karl så får du en gratis kopp choklad. (endast de 3 första som kommer till mig får detta, så du tjänar på att hålla denna funktionen hemlig tills efter att du fått din kopp)",
     "aaa": "Varför skriker du så mycket?",
     "aaaa": "Varför skriker du så mycket?",
