79971da24716b8f30c3fe11acf8aa589dd54596b
karlhenriksson <153213827+karlhenriksson@users.noreply.github.com>
2023-12-11 16:16:21 +0100

    Update mainProgram.js

--- mainProgram.js
+++ mainProgram.js
@@ -1,5 +1,5 @@
 "use strict";
-console.log("Version 1.3.3");
+console.log("Version 1.4");
 
 const mainCard = document.getElementById("mainCard");
 const inputField = document.getElementById("codeEntry");
